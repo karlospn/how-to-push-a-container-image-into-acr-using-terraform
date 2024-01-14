d2e782ba2f81e39b54b482aa4a5c7c2c284593ad
carlos pons <karlospn@gmail.com>
2024-01-14 14:07:08 +0100

    Remove https redirection

--- src/using_null_resource_with_docker_commands/init-app/Program.cs
+++ src/using_null_resource_with_docker_commands/init-app/Program.cs
@@ -2,8 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
 app.MapGet("/", async context =>
 {
     await context.Response.WriteAsync("That's a temporal image. Waiting for the proper image.");
